229c3de02a7430b0cd8d5122368c9c7043365408
Tal <prokish.tal@icloud.com>
2019-11-06 14:28:12 +0200

    tmp

---
+++ index.ts
@@ -0,0 +1,94 @@
+import express = require("express");
+import http = require("http");
+import io = require("socket.io");
+import fse = require("fs-extra");
+import path = require("path");
+import cheerio = require("cheerio");
+import request = require("request");
+import chance = require("chance");
+import logg = require("winston");
+
+const app: express.Application = express();
+const server: http.Server = new http.Server(app);
+const con: io.Server = io(server);
+const random: Chance.Chance = chance.Chance();
+
+let log = (type: string, content: string, file: string) => {
+  fse.appendFile(path.join(__dirname, file), content, (err) => {
+    if (err) throw err;
+    console.log(`${type} logged in ${file}`);
+  });
+};
+
+// CONNECT
+
+con.on("connection", (socket: any) => {
+  // connection log string
+  let content =
+    `[${socket.handshake.time}] ` +
+    `${socket.handshake.address} "` +
+    `${socket.handshake.headers["user-agent"]}" - New connection\n\n`;
+
+  log("Connection", content, "./connectionLog.txt");
+
+  // JSON
+
+  socket.on("receivedJsonFromClient", (data: any) => {
+    //send log string
+    let content =
+      `[${socket.handshake.time}] ` +
+      `${socket.handshake.address} "` +
+      `${socket.handshake.headers["user-agent"]}" - New JSON: ${data}, `;
+
+    data = JSON.parse(data);
+    let query = data.query.replace(" ", "+");
+    let url = `https://www.bing.com/search?q=${query}&qs=n&form=QBLH&sp=-1&pq=hello&sc=8-5&sk=&cvid=5ADDE9B642004421A77FC7EF634DDD8A`;
+
+    //request search
+    request(url, (res: any, body: any) => {
+      let $ = cheerio.load(body);
+      let results = $(".sb_count").text();
+      content += `${results}\n\n`;
+
+      log("JSON", content, "./sendLog.txt");
+    });
+  });
+
+  // RAW DATA
+
+  socket.on("receivedRawFromClient", (data: string) => {
+    let content =
+      `[${socket.handshake.time}] ` +
+      `${socket.handshake.address} "` +
+      `${socket.handshake.headers["user-agent"]}" - New RAW: (${data})\n\n`;
+
+    log("RAW", content, "./sendLog.txt");
+  });
+
+  // BINARY FILE
+
+  socket.on("receivedFileFromClient", (data: string) => {
+    let content =
+      `[${socket.handshake.time}] ` +
+      `${socket.handshake.address} "` +
+      `${socket.handshake.headers["user-agent"]}" - New file: ${socket.handshake.headers.referer}public/${data}\n\n`;
+
+    if (!fse.existsSync("./public")) {
+      fse.mkdir("./public");
+    }
+
+    fse.readFile(data, (err, buffer) => {
+      fse.writeFile("./public/test.jpg", buffer);
+    });
+
+    log("File", content, "./sendLog.txt");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+server.listen(5000, () => {
+  console.log("HTTP server started on port 3000");
+});
